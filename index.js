62d13247bf3e81d794563a4ec53d5c4f8131f85e
ahamadeh <hamadeh.bouvet@gmail.com>
2017-04-11 23:55:53 +0200

    dfddffd

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.use(express.static('ahmad'))
+app.use(express.static('ahmad'));
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
-})
+});
